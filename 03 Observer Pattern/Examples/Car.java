935a0a6a19c605739714f7a6f348cbcd08f37a51
MichaelViuff <128811537+MichaelViuff@users.noreply.github.com>
2026-02-09 09:51:06 +0100

    Modify Car class to handle additional traffic light states
    Updated light handling logic to include 'YELLOW_AND_RED' state and added error handling for broken traffic signals.

--- 03 Observer Pattern/Examples/Car.java
+++ 03 Observer Pattern/Examples/Car.java
@@ -14,21 +14,23 @@ public class Car
         {
             System.out.println("Car " + id + " drives");
         }
-        else if("YELLOW".equals(currentLight))
-        {
-            if("RED".equals(previousLight))
-            {
-                System.out.println("Car " + id + " turns engine on");
-            }
-            else
-            {
-                System.out.println("Car " + id + " slows down");
-            }
+        else if("YELLOW_AND_RED".equals(currentLight))
+        {                
+            System.out.println("Car " + id + " turns engine on");
         }
         else if("RED".equals(currentLight))
         {
             System.out.println("Car " + id + " stops");
         }
+        else if("YELLOW".equals(currentLight))
+        {
+            Sysem.out.println("Car " + id + " slows down");
+        }
+        else
+        {
+            System.out.println("Traffic signal broken...");
+        }
         previousLight = currentLight;
     }
 }
+
